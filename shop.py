88e91c0212a8cc2e1e3566cb47ebb83287babdc4
Іван Мотичка <ivanmotychka@MacBook-Pro.local>
2025-12-14 21:25:58 +0100

    Update site content

--- shop.py
+++ shop.py
@@ -2,40 +2,57 @@ import streamlit as st
 import os
 import time
 
-st.image("        images/icon.png", width=450)
-time.sleep(1)
+# Page config
+st.set_page_config(page_title="Amasoft", page_icon="💻")
 
+# Logo
+st.image("images/icon.png", width=300)
 
-st.title("                💻 Amasoft")
+# Loading effect
+with st.spinner("Starting Amasoft..."):
+    time.sleep(1)
 
+# Title
+st.markdown(
+    "<h1 style='text-align:center;'>💻 Amasoft</h1>",
+    unsafe_allow_html=True
+)
 
+# Typing effect
 text = "Welcome to Dagu Game"
-
 placeholder = st.empty()
 output = ""
 
 for char in text:
     output += char
-    placeholder.markdown(output)
+    placeholder.markdown(f"### {output}")
     time.sleep(0.05)
 
+st.divider()
 
+# Info
+st.write("📁 You can download games (more coming later)")
+st.write("🎮 You can now play Dagu or try the demo")
+st.write("❤️ Do you like my games?")
+
+# Debug / info
 st.write("Current directory:")
-st.write("You can down load games with will be added latter")
-st.write("You can now play dagu or open dagu demo")
-st.write("Do you like my games")
-st.write(os.getcwd())
+st.code(os.getcwd())
 
 st.write("Files in games/:")
-st.write(os.listdir("games"))
+st.code(os.listdir("games"))
 
+# Download button
 zip_path = os.path.join("games", "dagu_demo.zip")
 
-with open(zip_path, "rb") as file:
-    st.download_button(
-        label="Download for macOS",
-        data=file,
-        file_name="dagu_demo.zip",
-        mime="application/zip"
-    )
+if os.path.exists(zip_path):
+    with open(zip_path, "rb") as file:
+        st.download_button(
+            label="⬇️ Download for macOS",
+            data=file,
+            file_name="dagu_demo.zip",
+            mime="application/zip"
+        )
+else:
+    st.error("dagu_demo.zip not found!")
 
